7bea988089bc3cd9fa18ef8b9a81e234ea911498
starlying <corp@siteserver.cn>
2018-01-27 16:18:40 +0800

    properties

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SS.Payment")]
+[assembly: AssemblyDescription("SiteServer CMS Payment plugin.")]
+[assembly: AssemblyCopyright("Copyright © SiteServer CMS 2018")]
+[assembly: Guid("c9058996-734e-4557-8e6b-5b5ab9c3c506")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
